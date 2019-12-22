623595aaef846ab8d439f4421997fcf3a8596824
Christoph Settgast <csett86@web.de>
2019-12-22 18:57:18 +0100

    Update deps to update jetty
    update closure while at it
    
    clj-time is now separate, add it as well

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://my-personal-kanban.appspot.com"
   :license {:name "GNU General Public Licence version 3"
             :url "ttp://www.gnu.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.2"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [org.clojure/data.json "0.2.4"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [org.clojure/data.json "0.2.7"]
+                 [clj-time "0.15.2"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler mpk.core/mpk-app}
   :main mpk.core
   :prep-tasks[["compile" "mpk.action-handlers"]
